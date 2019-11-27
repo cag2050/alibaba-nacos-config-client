484635db63f25d224130f0f164f575755f16799b
chenanguo <cag2050@163.com>
2019-11-27 11:51:19 +0800

    project init

---
+++ src/main/java/com/example/alibabanacosconfigclient/AlibabaNacosConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.alibabanacosconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaNacosConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaNacosConfigClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alibabanacosconfigclient/AlibabaNacosConfigClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.alibabanacosconfigclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlibabaNacosConfigClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
